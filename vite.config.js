cc31c4e44468ab51002f745e825d5fe78e0ed7ba
Muhammad k <muhammedminu31@gmail.com>
2026-06-20 15:10:36 +0530

    fix vercel multipage build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        buy: resolve(__dirname, 'buy.html'),
+        category: resolve(__dirname, 'public/catogory.html'),
+      },
+    },
+  },
 })
